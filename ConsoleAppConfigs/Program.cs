3138cfe76aa994218174f98e9b8dfb34dd4b1245
Rafael de Mello Miceli Paiva <rafael.miceli@outlook.com.br>
2016-05-26 20:26:14 -0300

    Exemplo de multiplos configs

---
+++ ConsoleAppConfigs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppConfigs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trans = ConfigurationManager.AppSettings["transformar"];
+
+            Console.WriteLine(trans);
+
+            Console.Read();
+        }
+    }
+}
