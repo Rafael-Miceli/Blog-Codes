3c259c2d6c78b468397e3cc3e91c913d06af708d
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2016-06-15 22:35:18 -0300

    Auhtorization code grant configurado no startup

--- Escola/WebApp_AuthCode/Controllers/HomeController.cs
+++ Escola/WebApp_AuthCode/Controllers/HomeController.cs
@@ -13,42 +13,9 @@ namespace WebApp_AuthCode.Controllers
     {
         public ActionResult Index()
         {
-            //if (User.Identity.IsAuthenticated)
-            //{
-            //    return View();
-            //}
-
-            //var url = "http://localhost:54734/connect/authorize" +
-            //    "?client_id=teste_code" +
-            //    "&redirect_uri=http://localhost:60758/Home/AuthCallBack" +
-            //    "&response_type=code" +
-            //    "&scope=openid+profile" +
-            //    "&response_mode=form_post";
-
             return View();
-
         }
-
-        public ActionResult AuthCallBack(string code, string state, string error)
-        {
-            var tokenUrl = "http://localhost:54734/connect/token";
-
-            var client = new OAuth2Client(new Uri(tokenUrl), "teste_code", "secret");
-
-            var requestResult = client.RequestAccessTokenCode(code, new Uri("http://localhost:60758/Home/AuthCallBack"));
-
-            var claims = new[]
-            {
-                new Claim("access_token", requestResult.AccessToken)
-            };
-
-            var identity = new ClaimsIdentity(claims, "cookies");
-           
-            Request.GetOwinContext().Authentication.SignIn(identity); 
-
-            return Redirect("/");
-        }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
