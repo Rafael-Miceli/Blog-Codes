bf9ca4fe6fef76eb88493cc7aec6bb74adbc06a8
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-03-26 20:35:28 -0300

    Removido InMem Exemplo

--- InMemDb/src/InMemDb/Models/Client.cs
+++
@@ -1,9 +0,0 @@
-namespace InMemDb.Models
-{
-    public class Client
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Cnpj { get; set; }
-    }
-}
