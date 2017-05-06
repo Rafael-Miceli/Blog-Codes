7d7d6932a581524d15c330bfeee71730066db0ea
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-05-06 19:21:32 -0300

    Exemplo de aplicação WCF

---
+++ WcfApp/WcfApp.Contracts/IAlunoServiceContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfApp.Contracts
+{
+    [ServiceContract]
+    public interface IAlunoServiceContract
+    {
+        [OperationContract]
+        IEnumerable<string> BuscarNomesDosAlunos();
+    }
+}
---
+++ WcfApp/WcfApp.Services/AlunoServiceContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfApp.Contracts;
+
+namespace WcfApp.Services
+{
+    public class AlunoService : IAlunoServiceContract
+    {
+        public IEnumerable<string> BuscarNomesDosAlunos()
+        {
+            return new List<string> { "Aluno1", "Aluno2" };
+        }
+    }
+}
