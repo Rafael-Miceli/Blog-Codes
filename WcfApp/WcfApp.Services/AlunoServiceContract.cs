d74aff3dda95cef64797c70cc2699a9c257fdcd3
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-06-11 18:05:41 -0300

    Ground Ready para novo post

---
+++ WcfApp/WcfApp.Services/AlunoServiceContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfApp.Contracts;
+
+namespace WcfApp.Services
+{
+    public class AlunoService : IAlunoServiceContract
+    {
+        IAlunoApplicationService _alunoApplicationService;
+
+        public AlunoService(IAlunoApplicationService alunoApplicationService)
+        {
+            _alunoApplicationService = alunoApplicationService;
+        }
+
+        public IEnumerable<string> BuscarNomesDosAlunos()
+        {
+            return _alunoApplicationService.BuscarNomesDosAlunos();
+        }
+    }
+}
