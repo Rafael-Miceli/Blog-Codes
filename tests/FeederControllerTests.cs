82b1f8d128393646940f815ac2ea050d8be6fb1b
Rafael Miceli <rafael.miceli@hotmail.com>
2017-09-18 19:40:32 -0300

    Adicionado Moq e Feito testes para cobrir aplicação

--- tests/FeederControllerTests.cs
+++ tests/FeederControllerTests.cs
@@ -1,6 +1,10 @@
 using System;
 using Xunit;
 using api.Controllers;
+using Domain;
+using Moq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace tests
 {
@@ -10,30 +14,46 @@ namespace tests
         public void Given_A_Feeder_With_Empty_Name_When_Creating_The_Feeder_Then_Returns_BadRequest()
         {
             //Arrange            
-
+            var feederTest = new Feeder("");
+            var sut = new FeederController(null);
             //Act
+            var result = sut.Post(feederTest);
 
             //Assert
+            Assert.NotNull(result);
+            Assert.Equal(400, (result as BadRequestObjectResult).StatusCode);
         }
 
         [Fact]
         public void Given_A_Feeder_With_Existent_Name_In_DB_When_Creating_The_Feeder_Then_Returns_BadRequest()
         {
-            //Arrange
-
+            //Arrange       
+            var feederTest = new Feeder("Feeder Teste");     
+            var mockedRepository = new Mock<IFeederRepository>();
+            mockedRepository.Setup(x => x.GetAll()).Returns(new List<Feeder>{ new Feeder("Feeder Teste")});
+            var sut = new FeederController(new FeederService(mockedRepository.Object));
             //Act
+            var result = sut.Post(feederTest);
 
             //Assert
+            Assert.NotNull(result);
+            Assert.Equal(400, (result as BadRequestObjectResult).StatusCode);
         }
 
         [Fact]
         public void Given_A_Feeder_Valid_When_Creating_The_Feeder_Then_Returns_Created()
         {
-            //Arrange
-
+            //Arrange       
+            var feederTest = new Feeder("Feeder Teste 2");     
+            var mockedRepository = new Mock<IFeederRepository>();
+            mockedRepository.Setup(x => x.GetAll()).Returns(new List<Feeder>{ new Feeder("Feeder Teste")});
+            var sut = new FeederController(new FeederService(mockedRepository.Object));
             //Act
+            var result = sut.Post(feederTest);
 
             //Assert
+            Assert.NotNull(result);
+            Assert.Equal(201, (result as CreatedResult).StatusCode);
         }
     }
 }
