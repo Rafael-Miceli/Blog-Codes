95f17d255d0db2a56ecd97b93327f95996120080
Rafael Miceli <rafael.miceli@hotmail.com>
2017-09-18 19:16:32 -0300

    Preparando testes

---
+++ tests/FeederControllerTests.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using api.Controllers;
+
+namespace tests
+{
+    public class FeederControllerTests
+    {
+        [Fact]
+        public void Given_A_Feeder_With_Empty_Name_When_Creating_The_Feeder_Then_Returns_BadRequest()
+        {
+            //Arrange            
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void Given_A_Feeder_With_Existent_Name_In_DB_When_Creating_The_Feeder_Then_Returns_BadRequest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void Given_A_Feeder_Valid_When_Creating_The_Feeder_Then_Returns_Created()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
