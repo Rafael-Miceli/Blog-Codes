0e689ba090fbd2b6dfba2d109ddc8d24d91a105e
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-09-18 08:36:46 -0300

    Adicionando Infra de Mongo ao project mongodData

--- src/MongoData/FeederMongoRepository.cs
+++ src/MongoData/FeederMongoRepository.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using Domain;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoData
 {
     public class FeederMongoRepository: IFeederRepository
     {
+        private IMongoDatabase _mongoDb;
+        private IMongoCollection<Feeder> Feeders { get; set; }
+
+        public FeederMongoRepository()
+        {
+            InitializeMongoDatabase();
+            Feeders = _mongoDb.GetCollection<Feeder>("feeders");
+        }        
+
+        private void InitializeMongoDatabase()
+        {
+            try
+            {
+                var client = new MongoClient("mongodb://localhost:27017");
+                _mongoDb = client.GetDatabase("local");
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+                _mongoDb = null; 
+            }
+        }
+
         public IEnumerable<Feeder> GetAll()
         {
-            return new List<Feeder> {
-                new Feeder {
-                    Id = 1,
-                    Name = "Hello from Mongo"
-                }
-            };
+            return Feeders.Find(_ => true).ToList();
+
+            // return new List<Feeder> {
+            //     new Feeder {
+            //         Id = 1,
+            //         Name = "Hello from Mongo"
+            //     }
+            // };
+        }
+
+        public void AddFeeder(Feeder feeder)
+        {
+            Feeders.InsertOne(feeder);
         }
     }
 }
